7442911657326186caa9679b29bba6d47fd120d3
bashar0091 <awalbashar194@gmail.com>
2023-03-12 15:38:51 +0600

    new refresh app

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -11,15 +8,11 @@ use App\Http\Controllers\TodoController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-// front page route
-Route::get('/', [TodoController::class, 'index']);
-
-// todo list add route 
-Route::get('/todo/add', [TodoController::class, 'addIndex'])->name('todo.add');
-Route::post('/todo/add', [TodoController::class, 'save']);
-Route::get('/todo/delete/{id}', [TodoController::class, 'destroy']);
+Route::get('/', function () {
+    return view('welcome');
+});
